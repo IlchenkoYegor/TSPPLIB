0bba86dbb4559c93c33d75b8f5ef88f67d7769fd
dmitriy <dimalitvinenko2001@gmail.com>
2021-04-09 18:32:03 +0300

    Revert "1"
    This reverts commit f46ea7bdad0bd80b8a8a3e769ddfebdbeaca6363.

---
+++ TSPPLIB/model/SelectXX.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSPP2.model;
+
+namespace TSPPLIB.model
+{
+    class SelectXX
+    {
+        public static List<Book> SelectYear(List<Book> books ,int year)
+        {
+            return books.FindAll(e => (year == e.YearOfBook));
+        }
+    }
+}
