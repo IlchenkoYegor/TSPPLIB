e9f2d4b038235e5b5063904860704dc84d1124dd
dmitriy <dimalitvinenko2001@gmail.com>
2021-04-09 22:43:04 +0300

    LibraryForm, AddForm,EditForm

---
+++ TSPPLIB/view/AddForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace TSPPLIB.view
+{
+    partial class AddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxIdAdd = new System.Windows.Forms.TextBox();
+            this.label1Add = new System.Windows.Forms.Label();
+            this.label2Add = new System.Windows.Forms.Label();
+            this.label3Add = new System.Windows.Forms.Label();
+            this.label4Add = new System.Windows.Forms.Label();
+            this.label5Add = new System.Windows.Forms.Label();
+            this.textBoxNameAdd = new System.Windows.Forms.TextBox();
+            this.textBoxAuthorAdd = new System.Windows.Forms.TextBox();
+            this.textBoxYearAdd = new System.Windows.Forms.TextBox();
+            this.textBoxLocationAdd = new System.Windows.Forms.TextBox();
+            this.saveAddBtn = new System.Windows.Forms.Button();
+            this.cancelAddBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxIdAdd
+            // 
+            this.textBoxIdAdd.Location = new System.Drawing.Point(104, 31);
+            this.textBoxIdAdd.Name = "textBoxIdAdd";
+            this.textBoxIdAdd.Size = new System.Drawing.Size(55, 20);
+            this.textBoxIdAdd.TabIndex = 0;
+            // 
+            // label1Add
+            // 
+            this.label1Add.AutoSize = true;
+            this.label1Add.Location = new System.Drawing.Point(59, 34);
+            this.label1Add.Name = "label1Add";
+            this.label1Add.Size = new System.Drawing.Size(39, 13);
+            this.label1Add.TabIndex = 1;
+            this.label1Add.Text = "Шифр:";
+            // 
+            // label2Add
+            // 
+            this.label2Add.AutoSize = true;
+            this.label2Add.Location = new System.Drawing.Point(56, 91);
+            this.label2Add.Name = "label2Add";
+            this.label2Add.Size = new System.Drawing.Size(42, 13);
+            this.label2Add.TabIndex = 2;
+            this.label2Add.Text = "Назва:";
+            // 
+            // label3Add
+            // 
+            this.label3Add.AutoSize = true;
+            this.label3Add.Location = new System.Drawing.Point(58, 146);
+            this.label3Add.Name = "label3Add";
+            this.label3Add.Size = new System.Drawing.Size(40, 13);
+            this.label3Add.TabIndex = 3;
+            this.label3Add.Text = "Автор:";
+            // 
+            // label4Add
+            // 
+            this.label4Add.AutoSize = true;
+            this.label4Add.Location = new System.Drawing.Point(28, 200);
+            this.label4Add.Name = "label4Add";
+            this.label4Add.Size = new System.Drawing.Size(70, 13);
+            this.label4Add.TabIndex = 4;
+            this.label4Add.Text = "Рік видання:";
+            // 
+            // label5Add
+            // 
+            this.label5Add.AutoSize = true;
+            this.label5Add.Location = new System.Drawing.Point(15, 253);
+            this.label5Add.Name = "label5Add";
+            this.label5Add.Size = new System.Drawing.Size(83, 13);
+            this.label5Add.TabIndex = 5;
+            this.label5Add.Text = "Розташування:";
+            // 
+            // textBoxNameAdd
+            // 
+            this.textBoxNameAdd.Location = new System.Drawing.Point(104, 88);
+            this.textBoxNameAdd.Name = "textBoxNameAdd";
+            this.textBoxNameAdd.Size = new System.Drawing.Size(253, 20);
+            this.textBoxNameAdd.TabIndex = 6;
+            // 
+            // textBoxAuthorAdd
+            // 
+            this.textBoxAuthorAdd.Location = new System.Drawing.Point(104, 143);
+            this.textBoxAuthorAdd.Name = "textBoxAuthorAdd";
+            this.textBoxAuthorAdd.Size = new System.Drawing.Size(253, 20);
+            this.textBoxAuthorAdd.TabIndex = 7;
+            // 
+            // textBoxYearAdd
+            // 
+            this.textBoxYearAdd.Location = new System.Drawing.Point(104, 197);
+            this.textBoxYearAdd.Name = "textBoxYearAdd";
+            this.textBoxYearAdd.Size = new System.Drawing.Size(55, 20);
+            this.textBoxYearAdd.TabIndex = 8;
+            // 
+            // textBoxLocationAdd
+            // 
+            this.textBoxLocationAdd.Location = new System.Drawing.Point(104, 250);
+            this.textBoxLocationAdd.Name = "textBoxLocationAdd";
+            this.textBoxLocationAdd.Size = new System.Drawing.Size(55, 20);
+            this.textBoxLocationAdd.TabIndex = 9;
+            // 
+            // saveAddBtn
+            // 
+            this.saveAddBtn.Location = new System.Drawing.Point(27, 302);
+            this.saveAddBtn.Name = "saveAddBtn";
+            this.saveAddBtn.Size = new System.Drawing.Size(151, 23);
+            this.saveAddBtn.TabIndex = 10;
+            this.saveAddBtn.Text = "Зберегти";
+            this.saveAddBtn.UseVisualStyleBackColor = true;
+            // 
+            // cancelAddBtn
+            // 
+            this.cancelAddBtn.Location = new System.Drawing.Point(194, 302);
+            this.cancelAddBtn.Name = "cancelAddBtn";
+            this.cancelAddBtn.Size = new System.Drawing.Size(150, 23);
+            this.cancelAddBtn.TabIndex = 11;
+            this.cancelAddBtn.Text = "Скасувати";
+            this.cancelAddBtn.UseVisualStyleBackColor = true;
+            // 
+            // AddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 353);
+            this.Controls.Add(this.cancelAddBtn);
+            this.Controls.Add(this.saveAddBtn);
+            this.Controls.Add(this.textBoxLocationAdd);
+            this.Controls.Add(this.textBoxYearAdd);
+            this.Controls.Add(this.textBoxAuthorAdd);
+            this.Controls.Add(this.textBoxNameAdd);
+            this.Controls.Add(this.label5Add);
+            this.Controls.Add(this.label4Add);
+            this.Controls.Add(this.label3Add);
+            this.Controls.Add(this.label2Add);
+            this.Controls.Add(this.label1Add);
+            this.Controls.Add(this.textBoxIdAdd);
+            this.Name = "AddForm";
+            this.Text = "AddForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxIdAdd;
+        private System.Windows.Forms.Label label1Add;
+        private System.Windows.Forms.Label label2Add;
+        private System.Windows.Forms.Label label3Add;
+        private System.Windows.Forms.Label label4Add;
+        private System.Windows.Forms.Label label5Add;
+        private System.Windows.Forms.TextBox textBoxNameAdd;
+        private System.Windows.Forms.TextBox textBoxAuthorAdd;
+        private System.Windows.Forms.TextBox textBoxYearAdd;
+        private System.Windows.Forms.TextBox textBoxLocationAdd;
+        private System.Windows.Forms.Button saveAddBtn;
+        private System.Windows.Forms.Button cancelAddBtn;
+    }
+}
---
+++ TSPPLIB/view/LibraryForm.Designer.cs
@@ -0,0 +1,363 @@
+﻿
+namespace TSPPLIB.view
+{
+    partial class LibraryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(-3, -6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(958, 106);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.NavajoWhite;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.MediumBlue;
+            this.button1.Location = new System.Drawing.Point(788, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Вихід";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 74);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "КНИГОЛЮБ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Wheat;
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(-3, 89);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(946, 65);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Chocolate;
+            this.label2.Location = new System.Drawing.Point(259, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(454, 65);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "БІБЛІОТЕКА КНИГ";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Thistle;
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(733, 148);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(210, 411);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.ForeColor = System.Drawing.Color.DarkRed;
+            this.button6.Location = new System.Drawing.Point(32, 267);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(147, 51);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Зберегти зміни";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.ForeColor = System.Drawing.Color.DarkRed;
+            this.button5.Location = new System.Drawing.Point(32, 210);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(147, 51);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Видалити книгу";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.Color.DarkRed;
+            this.button4.Location = new System.Drawing.Point(32, 153);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(147, 51);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Редагувати запис про книгу";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.DarkRed;
+            this.button3.Location = new System.Drawing.Point(32, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 48);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Додати книгу";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.button2.Location = new System.Drawing.Point(21, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 65);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Пошук";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.groupBox4.Controls.Add(this.textBox3);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Controls.Add(this.textBox2);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Location = new System.Drawing.Point(-3, 148);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(742, 62);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(664, 26);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(66, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(588, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Рік видання:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(408, 26);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(174, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(362, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Автор:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Назва:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(295, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.button7.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.button7.Location = new System.Drawing.Point(21, 331);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(176, 65);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Зберегти в текстовий файл";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.Location = new System.Drawing.Point(-9, 210);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(748, 343);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Шифр";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 50;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Назва";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 300;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Автор";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 200;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Рік видання";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 70;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Місце знаходження";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 80;
+            // 
+            // LibraryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 556);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "LibraryForm";
+            this.Text = "LibraryForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
